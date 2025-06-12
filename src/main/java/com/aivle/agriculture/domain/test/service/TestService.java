3282c0ddd49b4efc9d75ad356ae7230550b2d6fc
leejongsuu <lik9819@naver.com>
2025-06-12 18:02:58 +0900

    refactor: Test 도메인 -> Service 수정

--- src/main/java/com/aivle/agriculture/domain/test/service/TestService.java
+++ src/main/java/com/aivle/agriculture/domain/test/service/TestService.java
@@ -1,4 +1,12 @@
 package com.aivle.agriculture.domain.test.service;
 
+import com.aivle.agriculture.domain.test.dto.TestRequest;
+import com.aivle.agriculture.domain.test.dto.TestResponse;
+
 public interface TestService {
+
+    TestResponse create(TestRequest request);
+
+    TestResponse find(Long id);
+    void update(Long id, TestRequest request);
 }
--- src/main/java/com/aivle/agriculture/domain/test/service/TestServiceImpl.java
+++ src/main/java/com/aivle/agriculture/domain/test/service/TestServiceImpl.java
@@ -13,25 +13,29 @@ import org.springframework.transaction.annotation.Transactional;
 import static com.aivle.agriculture.global.response.ErrorCode.NOT_FOUND;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final TestRepository testRepository;
     private final TestMapper testMapper;
 
+    @Override
     public TestResponse create(TestRequest request) {
         Test test = testMapper.toEntity(request);
         testRepository.save(test);
         return testMapper.toResponse(test);
     }
 
+    @Transactional(readOnly = true)
+    @Override
     public TestResponse find(Long id) {
         Test test = testRepository.findById(id)
                 .orElseThrow(() -> new CustomException(NOT_FOUND));
         return testMapper.toResponse(test);
     }
 
+    @Override
     public void update(Long id, TestRequest request) {
         Test test = testRepository.findById(id)
                 .orElseThrow(() -> new CustomException(NOT_FOUND));
