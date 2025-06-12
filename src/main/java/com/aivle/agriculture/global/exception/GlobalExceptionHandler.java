3f197dae024e11a3f87d3af207502b23e88ae9be
leejongsuu <lik9819@naver.com>
2025-06-12 17:46:27 +0900

    feat: 통일된 API 응답 구조 도입
    - SuccessCode, ErrorCode 분리하여 상태코드 명확히 구분
    - 공통 Response<T> 포맷 도입으로 응답 형식 일관화
    - ResponseFactory 통해 컨트롤러 응답 간결하게 처리
    - CustomException + GlobalExceptionHandler로 예외 응답 일원화

---
+++ src/main/java/com/aivle/agriculture/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.aivle.agriculture.global.exception;
+
+import com.aivle.agriculture.global.response.ErrorCode;
+import com.aivle.agriculture.global.response.Response;
+import com.aivle.agriculture.global.response.ResponseFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final ResponseFactory responseFactory;
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response<Void>> handleException(Exception e) {
+        log.error("서버 예외 발생 : {}", e.getMessage());
+        return responseFactory.error(new CustomException(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage()));
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Response<Void>> handleCustomException(CustomException e) {
+        log.error("커스텀 예외 발생 : {}", e.toString());
+        return responseFactory.error(e);
+    }
+}
