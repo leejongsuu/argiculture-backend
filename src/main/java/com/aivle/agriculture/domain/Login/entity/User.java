cb4b90c752c17c421deecff64a536ffd2b25e7c9
leejongsuu <lik9819@naver.com>
2025-06-25 23:53:02 +0900

    refactor: User 테이블 보안 강화
    - 생성자 접근 = PROTECTED로 바꿔서 불변성 지키기

--- src/main/java/com/aivle/agriculture/domain/Login/entity/User.java
+++ src/main/java/com/aivle/agriculture/domain/Login/entity/User.java
@@ -2,13 +2,14 @@ package com.aivle.agriculture.domain.Login.entity;
 
 import com.aivle.agriculture.global.base.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 public class User extends BaseEntity {
 
