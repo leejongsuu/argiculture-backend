1fd61675f413029afefb08542ee160ff05f8aa02
leejongsuu <lik9819@naver.com>
2025-06-25 20:26:21 +0900

    fix: merger과정 중 에러 수정

--- src/main/java/com/aivle/agriculture/global/config/WebConfig.java
+++ src/main/java/com/aivle/agriculture/global/config/WebConfig.java
@@ -1,16 +1,24 @@
 package com.aivle.agriculture.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private String frontendUrl;
+
+    public WebConfig(@Value("${external.frontend-url}") String frontendUrl) {
+        this.frontendUrl = frontendUrl;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 프론트엔드 주소
-                .allowedMethods("GET", "POST")
+                .allowedOrigins(frontendUrl) // 프론트엔드 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
--- src/main/java/com/aivle/agriculture/global/response/ErrorCode.java
+++ src/main/java/com/aivle/agriculture/global/response/ErrorCode.java
@@ -10,11 +10,6 @@ public enum ErrorCode {
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
-    
-    // TODO: 보험금 계산 개발 끝나면 지우기
-    // FastAPI 통신 관련 에러
-    FASTAPI_CONNECTION_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "보험금 계산 서비스에 연결할 수 없습니다."),
-    FASTAPI_CALCULATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "보험금 계산 중 오류가 발생했습니다."),
 
     // 5xx 서버 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
